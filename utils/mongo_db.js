d6c2eac5cbcd627507b36a541ab052c0dadd91d5
Nayan <nayanmevada@ymail.com>
2022-08-09 15:12:49 +0530

    - Adding mongo DB

---
+++ utils/mongo_db.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const DbConnection = function () {
+
+  const db = null;
+  const instance = 0;
+
+  async function DbConnect() {
+    try {
+      let url = '';
+      let _db = await MongoClient.connect(url);
+
+      return _db
+    } catch (e) {
+      return e;
+    }
+  }
+
+  async function Get() {
+    try {
+      instance++;     // this is just to count how many times our singleton is called.
+      console.log(`DbConnection called ${instance} times`);
+
+      if (db != null) {
+        console.log(`db connection is already alive`);
+        return db;
+      } else {
+        console.log(`getting new db connection`);
+        db = await DbConnect();
+        return db;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+
+  return {
+    Get: Get
+  }
+}
+
+module.exports = DbConnection();
